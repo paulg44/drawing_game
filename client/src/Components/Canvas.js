9c56e2cfe5d8a647d9bf5a056019aa82b15fc56e
Paul Garton <paulgarton1984@hotmail.com>
2024-11-05 20:38:52 +0000

    add basic canvas/image layout and add a more detailed component tree

---
+++ client/src/Components/Canvas.js
@@ -0,0 +1,11 @@
+import "../css/Canvas.css";
+
+function Canvas() {
+  return (
+    <div className="canvasContainer">
+      <h2>Canvas to draw on!</h2>
+    </div>
+  );
+}
+
+export default Canvas;
---
+++ client/src/Components/PictureDisplay.js
@@ -0,0 +1,11 @@
+import "../css/PictureDisplay.css";
+
+function PictureDisplay() {
+  return (
+    <div className="pictureDisplayContainer">
+      <h2>Picture will be displayed here!</h2>
+    </div>
+  );
+}
+
+export default PictureDisplay;
