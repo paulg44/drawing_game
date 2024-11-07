085d3b066c8f5f6b6ec2a5a3d429d0a0a7a0cd2f
Paul Garton <paulgarton1984@hotmail.com>
2024-11-07 21:06:44 +0000

    add a useState for when a user picks a category

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,22 +1,40 @@
 import { Link } from "react-router-dom";
 import kidsData from "../assets/data/kidsCategories.json";
 import colorData from "../assets/data/colours.json";
+import { useState } from "react";
 
 function Homepage() {
+  const [category, setCategory] = useState({});
+  const [color, setColor] = useState({});
   const mainCategories = Object.keys(kidsData.kidsCategories);
+
+  function handleCategorySelection(categoryName) {
+    const selectedCategory = kidsData.kidsCategories[categoryName];
+    setCategory({ name: categoryName, items: selectedCategory });
+  }
+
+  console.log(category);
+
   return (
     <div className="homepage">
       <h1>Welcome to Ivy's game!</h1>
       <h5>Pick a category</h5>
       <ul>
-        {mainCategories.map((category, index) => (
-          <li key={index}>{category}</li>
+        {mainCategories.map((categoryItem, index) => (
+          <button
+            key={index}
+            onClick={() => handleCategorySelection(categoryItem)}
+          >
+            <li>{categoryItem}</li>
+          </button>
         ))}
       </ul>
       <h5>Pen Colour</h5>
       <ul>
-        {colorData.colors.map((color) => (
-          <li key={color.id}>{color.name}</li>
+        {colorData.colors.map((colorItem) => (
+          <button key={colorItem.id}>
+            <li style={{ color: colorItem.hex }}>{colorItem.name}</li>
+          </button>
         ))}
       </ul>
       {/* Placeholders for now */}
