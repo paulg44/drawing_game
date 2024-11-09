3c70e01c7837a180dec1a8e3617b77feaf1e76c6
Paul Garton <paulgarton1984@hotmail.com>
2024-11-09 11:03:24 +0000

    pass users selection down to display side on game page

--- client/src/Pages/GamePage.js
+++ client/src/Pages/GamePage.js
@@ -1,12 +1,32 @@
+import { useLocation } from "react-router-dom";
 import "../assets/css/GamePage.css";
-import Canvas from "../Components/Canvas";
-import PictureDisplay from "../Components/PictureDisplay";
 
 function GamePage() {
+  const { state } = useLocation();
+  const category = state?.category;
+
+  console.log(category);
+
+  if (!category) {
+    return <div>No category selected</div>;
+  }
+
   return (
     <div className="gamePage">
-      <PictureDisplay />
-      <Canvas />
+      <div className="pictureDisplayContainer">
+        <h2>Display Side</h2>
+        {/* Replace with one random image */}
+        <ul>
+          {category.items.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="canvasContainer">
+        <h2>Canvas Side</h2>
+      </div>
+      {/* <PictureDisplay />
+      <Canvas /> */}
     </div>
   );
 }
--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,16 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import kidsData from "../assets/data/kidsCategories.json";
 import colorData from "../assets/data/colours.json";
 import { useState } from "react";
 
 function Homepage() {
   const [category, setCategory] = useState({});
-  const [color, setColor] = useState({});
+  // const [color, setColor] = useState({});
   const mainCategories = Object.keys(kidsData.kidsCategories);
+  const navigate = useNavigate();
 
-  function handleCategorySelection(categoryName) {
-    const selectedCategory = kidsData.kidsCategories[categoryName];
-    setCategory({ name: categoryName, items: selectedCategory });
+  function handleCategorySelection(categoryItem) {
+    const selectedCategory = kidsData.kidsCategories[categoryItem];
+    setCategory({ name: categoryItem, items: selectedCategory });
+  }
+
+  function startGame() {
+    if (category) {
+      navigate("/game", { state: { category } });
+    } else {
+      console.error("Category should be selected");
+    }
   }
 
   console.log(category);
@@ -38,7 +47,7 @@ function Homepage() {
         ))}
       </ul>
       {/* Placeholders for now */}
-      <Link to="/game">Game</Link>
+      <button onClick={startGame}>Start Game</button>
       <Link to="/score">Scores</Link>
     </div>
   );
