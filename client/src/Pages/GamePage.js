1b9a5e7ec0a1728e3ef29605c7bd1ff3254822b9
Paul Garton <paulgarton1984@hotmail.com>
2024-11-11 15:41:51 +0000

    replace basic konva example with Konva version of drawing pad: mouse down draws etc

--- client/src/Pages/GamePage.js
+++ client/src/Pages/GamePage.js
@@ -1,8 +1,38 @@
 import { useLocation } from "react-router-dom";
-import { Stage, Layer, Circle } from "react-konva";
+import { Stage, Layer, Circle, Text, Line } from "react-konva";
 import "../assets/css/GamePage.css";
+import { useRef, useState } from "react";
 
 function GamePage() {
+  // Canvas Side
+  const [tool, setTool] = useState("pen");
+  const [lines, setLines] = useState([]);
+  const isDrawing = useRef(false);
+
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
   // Display Side
   const { state } = useLocation();
   const category = state?.category;
@@ -29,11 +59,40 @@ function GamePage() {
       </div>
       <div className="canvasContainer">
         <h2>Canvas Side</h2>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
           <Layer>
-            <Circle x={100} y={100} radius={50} fill="red" draggable />
+            <Text text="Draw away!" x={5} y={30} />
+            {lines.map((line, i) => (
+              <Line
+                key={i}
+                points={line.points}
+                stroke="#df4b26"
+                strokeWidth={5}
+                tension={0.5}
+                lineCap="round"
+                lineJoin="round"
+                globalCompositeOperation={
+                  line.tool === "eraser" ? "destination-out" : "source-over"
+                }
+              />
+            ))}
           </Layer>
         </Stage>
+        <select
+          value={tool}
+          onChange={(e) => {
+            setTool(e.target.value);
+          }}
+        >
+          <option value="pen">Pen</option>
+          <option value="eraser">Eraser</option>
+        </select>
       </div>
     </div>
   );
