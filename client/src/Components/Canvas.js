56db07f5b480ca3afa9469b76c8c0deebf4ee789
Paul Garton <paulgarton1984@hotmail.com>
2024-12-22 21:31:27 +0000

    having a go at comparing the images. Think the problem is with converting them to the exact file type they need to be for each viable option

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -1,5 +1,6 @@
 import "../assets/css/Canvas.css";
 import { useEffect, useRef, useState } from "react";
+// import * as resemble from "resemblejs";
 import { HexColorPicker } from "react-colorful";
 import { Popup } from "reactjs-popup";
 import { Stage, Layer, Line } from "react-konva";
@@ -60,48 +61,78 @@ function Canvas({ randomItem }) {
     isDrawing.current = false;
   };
 
-  const handleSaveImage = async () => {
-    const userImageData = stageRef.current.toDataURL();
+  // I am trying two ways of saving and comparing images. This function uses a server, currently saves both the user image and random image to server. However I don't believe it's saving the random image in the correct format as it isn't visible like the users image is.
+  // const handleSaveImage = async () => {
+  //   const userImageData = stageRef.current.toDataURL();
 
-    const randomImageUrl = randomItem.image;
+  //   const randomImageUrl = randomItem.image;
 
-    try {
-      const response = await fetch(randomImageUrl);
-      const blob = await response.blob();
+  //   try {
+  //     const response = await fetch(randomImageUrl);
+  //     const blob = await response.blob();
 
-      const randomImageDataUrl = await convertToBase64(blob);
+  //     const randomImageDataUrl = await convertToBase64(blob);
 
-      const saveResponse = await fetch("/save-image", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userImage: userImageData,
-          randomImage: randomImageDataUrl,
-          metadata: randomItem,
-        }),
-      });
+  //     const saveResponse = await fetch("/save-image", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         userImage: userImageData,
+  //         randomImage: randomImageDataUrl,
+  //         metadata: randomItem,
+  //       }),
+  //     });
 
-      if (!response.ok) {
-        throw new Error(`Server error: ${response.statusText}`);
-      }
+  //     if (!response.ok) {
+  //       throw new Error(`Server error: ${response.statusText}`);
+  //     }
 
-      const data = await saveResponse.json();
-      console.log("Image saved successfully client:", data);
-    } catch (error) {
-      console.error("Error saving image client side:", error);
-    }
-  };
+  //     const data = await saveResponse.json();
+  //     console.log("Image saved successfully client:", data);
+  //   } catch (error) {
+  //     console.error("Error saving image client side:", error);
+  //   }
+  // };
 
-  const convertToBase64 = (blob) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  };
+  // const convertToBase64 = (blob) => {
+  //   return new Promise((resolve, reject) => {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => resolve(reader.result);
+  //     reader.onerror = reject;
+  //     reader.readAsDataURL(blob);
+  //   });
+  // };
+
+  // This is the client side version of the function above, converts the two images client side and compares them using resemblejs
+  // const handleCompareImages = async () => {
+  //   const userImageData = stageRef.current.toDataURL();
+  //   const randomImageUrl = randomItem.image;
+
+  //   const userImage = new Image();
+  //   const randomImage = new Image();
+
+  //   const userImageBlob = await dataURLToBlob(userImageData);
+  //   const userImageBlobURL = URL.createObjectURL(userImageBlob);
+
+  //   userImage.src = userImageBlobURL;
+  //   randomImage.src = randomImageUrl;
+
+  //   userImage.onload = () => {
+  //     randomImage.onload = () => {
+  //       resemble(userImage)
+  //         .compareTo(randomImage)
+  //         .ignoreColors()
+  //         .onComplete((data) => {
+  //           console.log(data);
+  //         });
+  //     };
+  //   };
+  // };
+  // const dataURLToBlob = async (dataURL) => {
+  //   return fetch(dataURL).then((res) => res.blob());
+  // };
 
   const handleClearPage = () => {
     setLines([]);
@@ -130,7 +161,8 @@ function Canvas({ randomItem }) {
           </div>
         </Popup>
 
-        <button onClick={handleSaveImage}>Save Image</button>
+        {/* <button onClick={handleSaveImage}>Save Image</button> */}
+        <button onClick={handleCompareImages}>Compare Images</button>
       </div>
       <Stage
         width={canvasSize.width}
