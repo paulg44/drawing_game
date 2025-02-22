9dca788b3cdab2d598a3395181564cdbd987ec92
Paul Garton <paulgarton1984@hotmail.com>
2025-02-22 10:53:07 +0000

    couple of small tweaks to ui while my son was testing: lots to do!

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -87,46 +87,46 @@ function Canvas({ randomItem }) {
     }
   };
 
-  const handleSaveImage = async () => {
-    // const convertToBase64 = (blob) => {
-    //   return new Promise((resolve, reject) => {
-    //     const reader = new FileReader();
-    //     reader.onloadend = () => resolve(reader.result);
-    //     reader.onerror = reject;
-    //     reader.readAsDataURL(blob);
-    //   });
-    // };
+  // const handleSaveImage = async () => {
+  //   // const convertToBase64 = (blob) => {
+  //   //   return new Promise((resolve, reject) => {
+  //   //     const reader = new FileReader();
+  //   //     reader.onloadend = () => resolve(reader.result);
+  //   //     reader.onerror = reject;
+  //   //     reader.readAsDataURL(blob);
+  //   //   });
+  //   // };
 
-    const userImageData = stageRef.current.toDataURL();
-    const base64String = userImageData.split(",")[1];
+  //   const userImageData = stageRef.current.toDataURL();
+  //   const base64String = userImageData.split(",")[1];
 
-    try {
-      console.log(
-        "User Image Base64 (first 50 chars):",
-        userImageData.slice(0, 50)
-      );
+  //   try {
+  //     console.log(
+  //       "User Image Base64 (first 50 chars):",
+  //       userImageData.slice(0, 50)
+  //     );
 
-      const saveResponse = await fetch("/save-image", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userImage: base64String,
-          metadata: randomItem,
-        }),
-      });
+  //     const saveResponse = await fetch("/save-image", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         userImage: base64String,
+  //         metadata: randomItem,
+  //       }),
+  //     });
 
-      if (!saveResponse.ok) {
-        throw new Error(`Server error: ${saveResponse.statusText}`);
-      }
+  //     if (!saveResponse.ok) {
+  //       throw new Error(`Server error: ${saveResponse.statusText}`);
+  //     }
 
-      const data = await saveResponse.json();
-      console.log("Image saved successfully client:", data);
-    } catch (error) {
-      console.error("Error saving image client side:", error);
-    }
-  };
+  //     const data = await saveResponse.json();
+  //     console.log("Image saved successfully client:", data);
+  //   } catch (error) {
+  //     console.error("Error saving image client side:", error);
+  //   }
+  // };
 
   const handleClearPage = () => {
     setLines([]);
@@ -155,10 +155,10 @@ function Canvas({ randomItem }) {
           </div>
         </Popup>
 
-        <button onClick={handleSaveImage}>Save Image</button>
+        {/* <button onClick={handleSaveImage}>Save Image</button> */}
         <button onClick={calculateScore}>Compare/Save Images</button>
-        <p>{score === null ? "Awaiting score..." : score}</p>
       </div>
+      <p>{score === null ? "Awaiting score..." : score}</p>
       <Stage
         width={canvasSize.width}
         height={canvasSize.height}
