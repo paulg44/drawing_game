3ee679e4f9b1ed79a43e999e253a7d9d8d914401
Paul Garton <paulgarton1984@hotmail.com>
2025-03-18 18:07:36 +0000

    remove trailing backslash

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors({ origin: "https://ivysgame.netlify.app/", credentials: true }));
+app.use(cors({ origin: "https://ivysgame.netlify.app", credentials: true }));
 
 // app.use(cors());
 app.options("*", cors());
