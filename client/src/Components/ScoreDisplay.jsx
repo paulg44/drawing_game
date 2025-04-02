f45da73eab04b4bc5d0b96ee7f485f8e3c1727b7
Paul Garton <paulgarton1984@hotmail.com>
2025-04-02 17:14:56 +0100

    create score display component

---
+++ client/src/Components/ScoreDisplay.jsx
@@ -0,0 +1,8 @@
+function ScoreDisplay() {
+  return (
+    // A component with three stars. A star for good, 2 for great, 3 for excellent when I receive the response from Gemini
+    <div className="scoreDisplay"></div>
+  );
+}
+
+export default ScoreDisplay;
