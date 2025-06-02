38d4b786e07f7213a0cbbd2d549f8f455821a2d8
Paul Garton <paulgarton1984@hotmail.com>
2025-06-02 21:44:10 +0100

    remove test mongo from production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import GamePage from "./Pages/GamePage";
 import { CategoryProvider } from "./context/CategoryContext";
-import MongoFetchTest from "./Components/testComponents/mongoFetchTest";
+// import MongoFetchTest from "./Components/testComponents/mongoFetchTest";
 import JustDrawPage from "./Pages/JustDrawPage";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/game" element={<GamePage />} />
           <Route path="/just-draw" element={<JustDrawPage />} />
-          <Route path="/fetchTest" element={<MongoFetchTest />} />
+          {/* <Route path="/fetchTest" element={<MongoFetchTest />} /> */}
         </Routes>
       </BrowserRouter>
     </CategoryProvider>
